7bc2ca8f487a61bbd602a02c6e915b48993931b1
sangwa7 <didiersangwa@gmail.com>
2022-04-13 16:34:53 +0200

    Refine Like feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
 import './mealDb.js';
 import './likeSection.js';
+import { Displaylikes } from './likeSection.js';
+import { like } from './likeSection.js';
+
+Displaylikes();
+like();
